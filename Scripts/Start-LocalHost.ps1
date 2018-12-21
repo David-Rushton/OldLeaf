435d1535e93d2a2d68e641581039c9c869ced422
David Rushton <david-rushton@outlook.com>
2018-12-21 23:33:36 +0000

    Added IIS installed check.

--- Scripts/Start-LocalHost.ps1
+++ Scripts/Start-LocalHost.ps1
@@ -6,4 +6,10 @@
     Starts OldLeaf.web, using IIS Express, on port 10101.
 #>
 
-    Start-Process -FilePath "C:\Program Files\IIS Express\IISExpress.exe" -ArgumentList "/path:C:\Users\Drushton\.dr\Repos\OldLeaf\Source\OldLeaf.web\wwwRoot /port:10101"
+
+    if ((Test-Path -Path 'C:\Program Files\IIS Express\IISExpress.exe') -eq $false) {
+
+        throw 'Cannot continue.  IIS Express required but not available.'
+    }
+
+    Start-Process -FilePath 'C:\Program Files\IIS Express\IISExpress.exe' -ArgumentList "/path:C:\Users\Drushton\.dr\Repos\OldLeaf\Source\OldLeaf.web\wwwRoot /port:10101"
